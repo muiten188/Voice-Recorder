8c9911dd7e81d3d949f226311190cf77546f569e
haivu <vu.longhai93@gmail.com>
2019-09-02 21:04:30 +0700

    Set full screen Splash Activity

--- android/app/src/main/java/com/haivu/voicerecorder/SplashActivity.java
+++ android/app/src/main/java/com/haivu/voicerecorder/SplashActivity.java
@@ -1,8 +1,13 @@
 package com.haivu.voicerecorder;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +15,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash);
         new Handler().postDelayed(new Runnable() {
             @Override
