3668c343c1f8563a57afa49638bd483384d9a9c2
haivu <vu.longhai93@gmail.com>
2019-09-03 21:35:04 +0700

    Update Drawer

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { COLORS } from '~/src/themes/common'
 import { View, Text, SmallButton } from '~/src/themes/ThemeComponent'
-import { Image, TouchableOpacity } from 'react-native'
+import { Image, TouchableOpacity, StyleSheet } from 'react-native'
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import I18n from '~/src/I18n'
 
@@ -59,28 +59,44 @@ export default class Drawer extends Component {
     render() {
         return (
             <View>
-                <View className='row-start ph16 border-bottom background' style={{ paddingTop: 22, paddingBottom: 14 }}>
+                <View className='row-start ph16 border-bottom background' style={styles.infoContainer}>
                     <Image source={{ uri: 'https://dsdigitaladvertisingdda.com/wp-content/uploads/2017/07/client.png' }}
-                        style={{ width: 42, height: 42, marginRight: 10, borderRadius: 21, borderWidth: 2, borderColor: COLORS.WHITE }}
-                    />
-                    <View>
-                        <Text className='textBlack s16 bold mb10'>Hải Vũ</Text>
-                        <Text className='gray'>vu.longhai93@gmail.com</Text>
-                    </View>
-                </View>
+                    style={styles.avatar}
+                />
                 <View>
-                    {DRAWER_MENUS.map(this._renderDrawerItem)}
-                    <View className='ph16 pv16 row-start'>
-                        <SmallButton
-                            red
-                            text={I18n.t('logout')}
-                            onPress={this._handlePressLogout}
-                        />
-                    </View>
-
+                    <Text className='textBlack s16 bold mb10'>Hải Vũ</Text>
+                    <Text className='gray'>vu.longhai93@gmail.com</Text>
                 </View>
+            </View>
+            <View>
+                <View className='space16' />
+                {DRAWER_MENUS.map(this._renderDrawerItem)}
+                <View className='ph16 pv16 row-start'>
+                    <SmallButton
+                        red
+                        text={I18n.t('logout')}
+                        onPress={this._handlePressLogout}
+                    />
+                </View>
+
+            </View>
             </View>
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+    avatar: {
+        width: 42,
+        height: 42,
+        marginRight: 10,
+        borderRadius: 21,
+        borderWidth: 2,
+        borderColor: COLORS.WHITE
+    },
+    infoContainer: {
+        paddingTop: 22,
+        paddingBottom: 14
+    }
+})
